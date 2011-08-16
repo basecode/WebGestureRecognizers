5a9f557ca9f48facd40469055129d59a70339c40
basecode <tag+github@basecode.de>
2011-08-17 01:07:23 +0200

    added several improvements to the recordMoves callback object. including position of the user interaction relative to the page and/or element.

--- recordMoves.js
+++ recordMoves.js
@@ -1,5 +1,6 @@
-// record moves
-(function(host/*host-object*/, doc/*document-object*/, undefined) {
+// onUserInteraction
+
+(function(host/*host-object*/, doc/*document-object*/) {
 
     "use strict";
     
@@ -8,8 +9,8 @@
 
     var INVALID_SESSION = 1;
     var VALID_SESSION   = 2;
-    var FIRST_RECORD    = 4;
-    var LAST_RECORD     = 8;
+    var FIRST_ACTION    = 4;
+    var LAST_ACTION     = 8;
 
     var m = function(sel, func) {
         this.callback = func;
@@ -20,9 +21,13 @@
     m.prototype.cons = {
         INVALID_SESSION : INVALID_SESSION,
         VALID_SESSION : VALID_SESSION,
-        FIRST_RECORD : FIRST_RECORD,
-        LAST_RECORD : LAST_RECORD
+        FIRST_ACTION : FIRST_ACTION,
+        LAST_ACTION : LAST_ACTION
     };
+    
+    m.prototype.getType = function(value) {
+        return Object.prototype.toString.apply(value).slice(8, -1).toLowerCase();
+    }
     
     m.prototype.cache = {
         x : 0,
@@ -30,27 +35,44 @@
         el: null
     };
     
-    m.prototype.getUserPosition = function(event) {
+    m.prototype.userPositionInPage = function(event) {
         return {
-            x : (event.touches) ? event.changedTouches[0].pageX : event.clientX,
-            y : (event.touches) ? event.changedTouches[0].pageY : event.clientY
+            x : (event.touches) ? event.changedTouches[0].pageX : event.pageX,
+            y : (event.touches) ? event.changedTouches[0].pageY : event.pageY
+        };
+    }
+    
+    m.prototype.userPositionInElement = function(inPagePosition) {
+        var obj = this.cache.element, left = 0, top = 0;
+        
+        do {
+            left += obj.offsetLeft;
+			top += obj.offsetTop;
+        } while(obj = obj.offsetParent);
+        
+        return {
+            x : inPagePosition.x - left,
+            y : inPagePosition.y - top
         };
     }
 
     m.prototype.validateSession = function(e) {
+        
         if (/touchstart|mousedown/.test(e.type)) { /* start session */
-            this.cache.x = this.getUserPosition(e).x;
-            this.cache.y = this.getUserPosition(e).y;
-            this.cache.element = e.target;
-            this.info = (this.info | VALID_SESSION | FIRST_RECORD) & ~INVALID_SESSION;
+            this.firstAction(e);
         } else {
-            if (this.info & VALID_SESSION && this.info & FIRST_RECORD) { /*bitwise solution? */
-                this.info &= ~FIRST_RECORD;
+            if (this.info & FIRST_ACTION) {
+                this.info &= ~FIRST_ACTION;
             }
         }
-        
+
+        if (!(this.info & VALID_SESSION)) {
+            // session is invalid
+            return false;
+        }
+
         if (/touchend|touchcancel|mouseup/.test(e.type)) { /* end session */
-            this.info = (this.info | INVALID_SESSION | LAST_RECORD) & ~VALID_SESSION;
+            this.lastAction();
         }
 
         return this.info & VALID_SESSION;
@@ -62,45 +84,70 @@
     
     m.prototype.moving = function(e) {
         
-        if (!this.validateSession(e) && !(this.info & LAST_RECORD)) {
+        if (!this.validateSession(e) && !(this.info & LAST_ACTION)) {
             return;
         }
 
         var m = this;
+        var inPagePosition = this.userPositionInPage(e);
+        var inElementPosition = this.userPositionInElement(inPagePosition);
         
         // diff to the last position
         var delta = {
-            x : this.getUserPosition(e).x - this.cache.x,
-            y : this.getUserPosition(e).y - this.cache.y
+            x : inPagePosition.x - this.cache.x,
+            y : inPagePosition.y - this.cache.y
         }
 
         // cache values
-        this.cache.x = this.getUserPosition(e).x;
-        this.cache.y = this.getUserPosition(e).y;
+        this.cache.x = inPagePosition.x;
+        this.cache.y = inPagePosition.y;
 
         // call callback
-        // delta will be 0 on last record
+        // delta will be 0 on last action
         this.callback({
             element : this.cache.element,
-            delta   : delta,
-            isFirstRecord : function() {
-                return !!(m.info & FIRST_RECORD);
+            position : {
+                page : inPagePosition,
+                element : inElementPosition,
+                delta : delta
             },
-            isLastRecord : function() {
-                return !!(m.info & LAST_RECORD);
+            isFirstAction : function() {
+                return !!(m.info & FIRST_ACTION);
+            },
+            isLastAction : function() {
+                return !!(m.info & LAST_ACTION);
             },
             event : e
         });
         
-        // remove flag LAST_RECORD
-        this.info &= ~LAST_RECORD;
+        // remove flag LAST_ACTION
+        this.info &= ~LAST_ACTION;
+    }
+    
+    m.prototype.firstAction = function(e) {
+        var pagePosition = this.userPositionInPage(e);
+        this.cache.x = pagePosition.x;
+        this.cache.y = pagePosition.y;
+        
+        if (this.getType(e.target.offsetParent) == "undefined") {
+            throw new Error("DOM Element 'offsetParent' is not supported.");
+        }
+        
+        this.cache.element = e.target;
+        
+        this.info = (this.info | VALID_SESSION | FIRST_ACTION) & ~INVALID_SESSION;
+    }
+    
+    m.prototype.lastAction = function() {
+        this.info = (this.info | INVALID_SESSION | LAST_ACTION) & ~VALID_SESSION;
     }
     
     m.prototype.attachEventListeners = function(sel) {
+        var selType = this.getType(sel);
         var eve = "addEventListener";
-        var els = doc.querySelectorAll(sel);
+        var els = selType == "string" ? doc.querySelectorAll(sel)/*selector*/ : /element$/.test(selType) ? [sel]/*html-element*/ : sel/*nodelist*/;
         var m   = this;
-        var callback = function(e) {
+        var action = function(e) {
             e.preventDefault();
             m.moving(e);
         }
@@ -109,17 +156,17 @@
             return;
         
         for (var i = 0, len = els.length; i < len; i++) {
-            els[i][eve]("ontouchstart" in host ? "touchstart" : "mousedown", callback, false);
+            els[i][eve]("ontouchstart" in host ? "touchstart" : "mousedown", action, false);
         }
 
-        host[eve]("ontouchmove" in host ? "touchmove" : "mousemove", callback, false);
-        host[eve]("ontouchend" in host ? "touchend" : "mouseup", callback, false);
-        host[eve]("ontouchcancel" in host ? "touchcancel" : "mouseup", callback, false);
+        host[eve]("ontouchmove" in host ? "touchmove" : "mousemove", action, false);
+        host[eve]("ontouchend" in host ? "touchend" : "mouseup", action, false);
+        host[eve]("ontouchcancel" in host ? "touchcancel" : "mouseup", action, false);
         
         return this;
     }
 
-    host.recordMoves = function(selector, callback) {
+    host.onUserInteraction = function(selector, callback) {
         return new m(selector, callback);
     }
 
